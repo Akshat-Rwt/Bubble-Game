8411fb2ef1072d997e720fd08fffc4f3e94682ba
Akshat Rawat <akshatrawat84@gmail.com>
2024-04-21 18:00:45 +0530

    Clutter
    1.Make a Variable Clutter .
    2.Make a For Loop .
    3.Make clutter = clutter + bubble .
    
    But we use ` this symbol which is BACKTICK.
    
    Now its time to make DOM and by using that thing we can make multiple
    bubble .

---
+++ script.js
@@ -0,0 +1,17 @@
+var clutter = "" ;
+
+
+for(var i = 1 ; i <= 133 ; i++){
+
+   clutter += `<div class="bubble">5</div>`;
+   
+   /* 
+      '' => Single Quote
+      `` => BackTick  
+      Both are used in string . 
+
+   */
+
+}
+
+document.querySelector("#pbtm").innerHTML = clutter ;
